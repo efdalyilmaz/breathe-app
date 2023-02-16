1c17777cdebf34f504f0f06e9ce319ec8ba1cac3
Efdal Yılmaz <yilmazefdal@gmail.com>
2023-02-16 23:41:06 +0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Breathe } from './src/screens/Breathe';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return (<Breathe />);
 }
 
 const styles = StyleSheet.create({
